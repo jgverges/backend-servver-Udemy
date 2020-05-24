0b93240b74cdfa5cbc443b60be06e2ad8a0c791c
GONZAL17 <jordi.conzalez@kapsch.net>
2020-05-24 19:40:17 +0200

    124

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -4,6 +4,7 @@ var app = express();
 
 var Hospital = require('../models/hospital');
 var Medico = require('../models/medico');
+var Usuario = require('../models/usuario');
 
 
 app.get('/todo/:busqueda', (req, res, next) => {
@@ -12,37 +13,78 @@ app.get('/todo/:busqueda', (req, res, next) => {
     var regex = new RegExp( busqueda, 'i');
 
 
-    buscarHospitales( busqueda, regex )
-        .then( hospitales => {
+    Promise.all( [
+         buscarHospitales( busqueda, regex),
+         buscarMedicos( busqueda, regex), 
+         buscarUsuarios( busqueda, regex) 
+        ])
+        .then( respuestas  => {
 
             res.status(200).json({
                 ok: true,
-                hospitales:hospitales
+                hospitales: respuestas[0],
+                medicos: respuestas[1],
+                usuarios: respuestas[2]
              });
-        
-        });
+        })
 
     
 });
+
 
 function buscarHospitales (busqueda, regex){
 
     return new Promise( (resolve, reject) =>{
 
-        Hospital.find({nombre:regex}, (err, hospitales) =>{
+        Hospital.find({nombre:regex})
+            .populate('usuario', 'nombre email')
+            .exec( (err, hospitales) =>{
 
                 if(err) {
                     reject('Error al cargar hospitales', err);
                 }else{
                     resolve(hospitales)
                 }
-        });
+            });
+    });   
+}
+
+function buscarMedicos (busqueda, regex){
+
+    return new Promise( (resolve, reject) =>{
+
+        Medico.find({nombre:regex})
+                .populate('usuario', 'nombre email')
+                .populate('hospital')
+                .exec( (err, medicos) =>{
+
+                    if(err) {
+                        reject('Error al cargar hospitales', err);
+                    }else{
+                        resolve(medicos)
+                    }
+                });
+    });   
+}
+
+function buscarUsuarios (busqueda, regex){
+
+    return new Promise( (resolve, reject) =>{
+
+        Usuario.find({}, 'nombre email role')
+                .or( [ { 'nombre': regex}, { 'email': regex}])
+                .exec( (err,usuarios) => {
+
+                    if (err){
+                        reject('Error al cargar usuarios', err);
+                    }else{
+                        resolve( usuarios);
+                    }
 
 
+                });
 
-    });
-
-    
+    });    
 }
 
 module.exports = app;
